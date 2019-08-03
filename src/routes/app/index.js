9242558b82736a0bea01dc3392a4370d544a2ddb
Łukasz Wójcik <lwojcik@lukem.net>
2019-08-03 14:19:37 +0200

    100% coverage

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -13,157 +13,101 @@ router.get('/', (req, res) => {
 });
 
 router.get("/queryStaticProfileData/:regionId", async (req, res) => {
-  try {
-    const { regionId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryStaticProfileData(regionId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryStaticProfileData(regionId);
+  res.json(response);
 });
 
 router.get("/queryProfileMetadata/:regionId/:realmId/:profileId", async (req, res) => {
-  try {
-    const { regionId, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryProfileMetadata({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryProfileMetadata({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryProfile/:regionId/:realmId/:profileId", async (req, res) => {
-  try {
-    const { regionId, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryProfile({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryProfile({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryLadderSummary/:regionId/:realmId/:profileId", async (req, res) => {
-  try {
-    const { regionId, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLadderSummary({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLadderSummary({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryPlayerLadder/:regionId/:realmId/:profileId/:ladderId", async (req, res) => {
-  try {
     const { regionId, realmId, profileId, ladderId } = req.params;
     const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
     const response = await Sc2API.queryPlayerLadder({ regionId, realmId, profileId }, ladderId);
     res.json(response);
-  } catch (error) {
-    throw error;
-  }
 });
 
 router.get("/queryGrandmasterLeaderboard/:regionId", async (req, res) => {
-  try {
-    const { regionId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryGrandmasterLeaderboard(regionId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryGrandmasterLeaderboard(regionId);
+  res.json(response);
 });
 
 router.get("/querySeason/:regionId", async (req, res) => {
-  try {
-    const { regionId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.querySeason(regionId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.querySeason(regionId);
+  res.json(response);
 });
 
-router.get("/queryPlayerAccount/:accountId", async (req, res) => {
-  try {
-    const { accountId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryPlayerAccount(accountId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+router.get("/queryPlayerAccount/:regionId/:accountId", async (req, res) => {
+  const { regionId, accountId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryPlayerAccount(accountId);
+  res.json(response);
 });
 
 router.get("/queryLegacyProfile/:regionId/:realmId/:profileId", async (req, res) => {
-  try {
-    const { regionId, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyProfile({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyProfile({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryLegacyLadders/:regionId/:realmId/:profileId", async (req, res) => {
-  try {
-    const { regionId, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyLadders({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyLadders({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryLegacyMatchHistory/:region/:realmId/:profileId", async (req, res) => {
-  try {
-    const { region, realmId, profileId } = req.params;
-    const Sc2API = new StarCraft2API({ region, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyMatchHistory({ regionId, realmId, profileId });
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, realmId, profileId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyMatchHistory({ regionId, realmId, profileId });
+  res.json(response);
 });
 
 router.get("/queryLegacyLadder/:regionId/:ladderId", async (req, res) => {
-  try {
-    const { regionId, ladderId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyLadder(regionId, ladderId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId, ladderId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyLadder(regionId, ladderId);
+  res.json(response);
 });
 
 router.get("/queryLegacyAchievements/:regionId", async (req, res) => {
-  try {
-    const { regionId } = req.params;
-    const Sc2API = new StarCraft2API({ regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyAchievements(regionId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId } = req.params;
+  const Sc2API = new StarCraft2API({ regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyAchievements(regionId);
+  res.json(response);
 });
 
 router.get("/queryLegacyRewards/:regionId", async (req, res) => {
-  try {
-    const { regionId } = req.params;
-    const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
-    const response = await Sc2API.queryLegacyRewards(regionId);
-    res.json(response);
-  } catch (error) {
-    throw error;
-  }
+  const { regionId } = req.params;
+  const Sc2API = new StarCraft2API({ region: regionId, clientId, clientSecret });
+  const response = await Sc2API.queryLegacyRewards(regionId);
+  res.json(response);
 });
 
 module.exports = router;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,85 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-describe('/', () => {
-  it('should return 200', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
+const regionId = '1';
+const realmId = '1';
+const profileId = '123456';
+const ladderId = '234567';
+const accountId = '123';
 
-  it('should match snapshot', async () => {
-    const response = await request(app).get('/');
-    expect(response.body).toMatchSnapshot();
+jest.mock('starcraft2-api', () => jest.fn().mockImplementation(() => ({
+  queryStaticProfileData: () => Promise.resolve({
+    data: 'sample data for queryStaticProfileData',
+  }),
+  queryProfileMetadata: () => Promise.resolve({
+    data: 'sample data for queryProfileMetadata',
+  }),
+  queryProfile: () => Promise.resolve({
+    data: 'sample data for queryProfile',
+  }),
+  queryLadderSummary: () => Promise.resolve({
+    data: 'sample data for queryLadderSummary',
+  }),
+  queryPlayerLadder: () => Promise.resolve({
+    data: 'sample data for queryPlayerLadder',
+  }),
+  queryGrandmasterLeaderboard: () => Promise.resolve({
+    data: 'sample data for queryGrandmasterLeaderboard',
+  }),
+  querySeason: () => Promise.resolve({
+    data: 'sample data for querySeason',
+  }),
+  queryPlayerAccount: () => Promise.resolve({
+    data: 'sample data for queryPlayerAccount',
+  }),
+  queryLegacyProfile: () => Promise.resolve({
+    data: 'sample data for queryLegacyProfile',
+  }),
+  queryLegacyLadders: () => Promise.resolve({
+    data: 'sample data for queryLegacyLadders',
+  }),
+  queryLegacyMatchHistory: () => Promise.resolve({
+    data: 'sample data for queryLegacyMatchHistory',
+  }),
+  queryLegacyLadder: () => Promise.resolve({
+    data: 'sample data for queryLegacyLadder',
+  }),
+  queryLegacyAchievements: () => Promise.resolve({
+    data: 'sample data for queryLegacyAchievements',
+  }),
+  queryLegacyRewards: () => Promise.resolve({
+    data: 'sample data for queryLegacyRewards',
+  }),
+})));
+
+const routes = [
+  '/',
+  `/queryStaticProfileData/${regionId}`,
+  `/queryProfileMetadata/${regionId}/${realmId}/${profileId}`,
+  `/queryProfile/${regionId}/${realmId}/${profileId}`,
+  `/queryLadderSummary/${regionId}/${realmId}/${profileId}`,
+  `/queryPlayerLadder/${regionId}/${realmId}/${profileId}/${ladderId}`,
+  `/queryGrandmasterLeaderboard/${regionId}`,
+  `/querySeason/${regionId}`,
+  `/queryPlayerAccount/${regionId}/${accountId}`,
+  `/queryLegacyProfile/${regionId}/${realmId}/${profileId}`,
+  `/queryLegacyLadders/${regionId}/${realmId}/${profileId}`,
+  `/queryLegacyMatchHistory/${regionId}/${realmId}/${profileId}`,
+  `/queryLegacyLadder/${regionId}/${ladderId}`,
+  `/queryLegacyAchievements/${regionId}`,
+  `/queryLegacyRewards/${regionId}`,
+];
+
+routes.map(route => {
+  describe(route, () => {
+    test('should return 200', async () => {
+      const response = await request(app).get(route);
+      expect(response.statusCode).toBe(200);
+    });
+
+    test('should match snapshot', async () => {
+      const response = await request(app).get(route);
+      expect(response.body).toMatchSnapshot();
+    });
   });
 })
